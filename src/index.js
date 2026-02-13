10260eab901d50fe990e789b74546b1df44d9dd2
lou-bou <boussaid.louai25@gmail.com>
2026-02-13 21:31:10 +0100

    Implement task creation functionality
    - Create task class with attributes
    - Create task constructor (creation functionality)
    - Separate adding categories logic to list from class
    - Persistance not yet implemented for this functionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log("hello")
+class Task {
+    id;
+    title;
+    categories = [];
+    status;
+
+    constructor(title, categories) { // the categories parameter here must be implemented as an array
+        this.id = crypto.randomUUID();
+        this.title = title;
+        this.categories = addCategories(this.categories, categories);
+        this.status = 'pending';
+    }
+}
+
+function addCategories(categorieslist, categories) {
+    for (let i = 0; i < categories.length; i++) {
+        categorieslist.push(categories[i]);
+    }
+
+    return categorieslist;
+}
